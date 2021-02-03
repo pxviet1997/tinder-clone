f4c0950e01db393bfdf15b215d073becce83946b
Viet Pham <pxviet1997@gmail.com>
2021-02-03 13:18:19 +1100

    change name from tinder-clone to tinder-frontend

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,2 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+
+// Middlewares
+
+// DB Config
+
+// API Endpoints
+app.get('/', (req, res) => {
+  res.status(200)
+    .send('HELLO');
+});
+
+// Listener
+app.listen(port, () => {
+  console.log(`listening on localhost: ${port}`);
+})
