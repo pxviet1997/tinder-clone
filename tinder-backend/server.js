64f921e0aa0ac4149cae3deedeb0b37605745bac
Viet Pham <pxviet1997@gmail.com>
2021-02-03 14:36:50 +1100

    create cardSchema

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -4,10 +4,16 @@ import mongoose from 'mongoose';
 // App Config
 const app = express();
 const port = process.env.PORT || 8001;
+const connectionUrl = "mongodb+srv://admin:padpxv9697@cluster0.ksgce.mongodb.net/tinderDB?retryWrites=true&w=majority";
 
 // Middlewares
 
 // DB Config
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 app.get('/', (req, res) => {
